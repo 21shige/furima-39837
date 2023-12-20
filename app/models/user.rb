1b201ad67854677a2a9c485f19ecfbb27384ad1a
21shige <146493286+21shige@users.noreply.github.com>
2023-12-20 22:03:50 +0900

    userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d).+\z/ ,message: " is invalid. Include both letters and numbers"}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ ,message: "is invalid. Input full-width characters"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ ,message: "is invalid. Input full-width characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ ,message: "is invalid. Input full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ ,message: "is invalid. Input full-width katakana characters"}
+  validates :birthday, presence: true
 end
