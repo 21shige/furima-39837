1062a5dbd5a6ae0bc495d37920c6416120eb824d
21shige <146493286+21shige@users.noreply.github.com>
2023-12-28 22:14:00 +0900

    半角英数混合の値を生成するように修正しました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname   { Faker::Name.initials(number: 2) }
     email      { Faker::Internet.email }
-    password   { Faker::Internet.password(min_length: 6) }
+    password   { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     last_name  { generate_japanese_last_name }
     first_name { generate_japanese_first_name }
