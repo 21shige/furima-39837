5ccfc2d1a356a2a188fbea463fb5508d95c4e21d
21shige <146493286+21shige@users.noreply.github.com>
2024-01-06 12:37:56 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :cost_burden
+  belongs_to :prefecture
   belongs_to :shipping_days
 
   has_many :comments
@@ -10,6 +11,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
  
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}  
